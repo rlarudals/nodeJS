936054dc16b4ebdeaae2aefe771a6f3a6b27e33e
KimKyoungMin <gim181004@gmail.com>
2019-12-21 15:32:41 +0900

    mixins setting, dotenv, pug

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 import express from "express";
+// import dotenv from "dotenv";
 
 const app = express();
 
+// dotenv.config();
+
+// 1. 확장자를 .pug로 검색한다.
+// 2. "views" 폴더를 default 경로로 자동 인식 한다.
+
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-  res.send("Hello Node");
+  res.render("home");
 });
 
-app.get("/gh", (req, res) => {
-  res.send("This page is GH page!");
+app.get("/post", (req, res) => {
+  res.render("post");
 });
 
 app.listen(3000, () => console.log("server start"));
